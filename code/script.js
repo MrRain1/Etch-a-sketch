f6b7edc41018652e98d366411258a44881ef2544
MrRain1 <b.degliuberti@outlook.com>
2022-04-20 11:28:36 +0200

    Completed grid and attached listeners

--- code/script.js
+++ code/script.js
@@ -3,11 +3,14 @@ let num=3;
 
 const container = document.querySelector(`.grid-container`);
 const clearButton = document.querySelector(`.clearGrid`);
+
+//initialize the nodelist to attach the eventlisteners
+let gridElements = document.querySelectorAll(".gridElement");
+
+//generate the default grid
 generateGrid(defaultColumnSize**2);
 
-clearButton.addEventListener('click', function(){clearGrid(defaultColumnSize**2)});
-
-
+clearButton.addEventListener('click', function(){clearGrid(num**2)});
 
 function generateGrid(size){
     let colNumAuto = "";
@@ -22,17 +25,30 @@ function generateGrid(size){
         gridElement.classList.toggle("gridElement");
     }
     container.style["grid-template-columns"] = colNumAuto;
+
+    //Call function to link the listener to the cells
+    attachListener();
 }
 
 function clearGrid(size){
     let colNumAuto = "";
-    for(let i=0; i < container.length; i++){
-        const gridElement = document.createElement(`div`);
+
+    while(container.childElementCount>0){
+        const gridElement = document.querySelector(`.gridElement`);
         container.removeChild(gridElement);
     }
-
+    
     container.style["grid-template-columns"] = colNumAuto;
 
     generateGrid(size);
 }
 
+//function to toggle the css change
+function onOver(cellElement){
+    cellElement.classList.toggle("gridElementHovered");
+}
+
+function attachListener(){
+    gridElements = document.querySelectorAll(".gridElement");
+    gridElements.forEach(cell => cell.addEventListener("mouseover", function(){onOver(cell)}));
+}
